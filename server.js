1f8a02d3233d5dca2684d0afdd7b8f4eaba7f2b4
Sherlock5063 <Sherlock5063>
2023-01-23 15:51:26 -0400

    rename 'index' file for 'server'

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+    const file = req.url === "/" ? "index.html" : req.url;
+    const filePath = path.join(__dirname, "public", file);
+    const extname = path.extname(filePath);
+
+    const allowedFileTypes = ['.html', '.css', '.js'];
+    const allowed = allowedFileTypes.find(item => item == extname);
+
+    if(!allowed) return;
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) throw err;
+        res.end(data);
+    });
+});
+
+server.listen(port, console.log(`server is running!`));
