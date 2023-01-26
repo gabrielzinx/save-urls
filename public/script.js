48fdcb9bf6b16924faa81bdf17228c2d6d1e457b
Gabriel <88208371+sherlock5063@users.noreply.github.com>
2023-01-25 21:26:49 -0400

    adding semicolons to the file

--- public/script.js
+++ public/script.js
@@ -1,57 +1,57 @@
-const ul = document.querySelector("ul")
-const input = document.querySelector("input")
-const form = document.querySelector('form')
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const form = document.querySelector('form');
 
 async function load() {
-    const content = await fetch("http://localhost:3000/").then((data) => data.json())
+    const content = await fetch("http://localhost:3000/").then((data) => data.json());
     
-    content.urls.map(({name, url}) => addElement({name, url}))
+    content.urls.map(({name, url}) => addElement({name, url}));
 };
 
-load()
+load();
 
 function addElement({ name, url }) {
-    const li = document.createElement('li')
-    const a = document.createElement("a")
-    const trash = document.createElement("span")
+    const li = document.createElement('li');
+    const a = document.createElement("a");
+    const trash = document.createElement("span");
 
-    a.href = url
-    a.innerHTML = name
-    a.target = "_blank"
+    a.href = url;
+    a.innerHTML = name;
+    a.target = "_blank";
 
-    trash.innerHTML = "x"
-    trash.onclick = () => removeElement(trash)
+    trash.innerHTML = "x";
+    trash.onclick = () => removeElement(trash);
 
-    li.append(a)
-    li.append(trash)
-    ul.append(li)
+    li.append(a);
+    li.append(trash);
+    ul.append(li);
 }
 
 function removeElement(el) {
     if (confirm('Tem certeza que deseja deletar?')) {
         const name = el.parentNode.childNodes[0].innerText;
         const url = el.parentNode.childNodes[0].href.slice(0, -1);
-        fetch(`http://localhost:3000/?name=${name}&url=${url}&del=true`).then(el.parentNode.remove())
+        fetch(`http://localhost:3000/?name=${name}&url=${url}&del=true`).then(el.parentNode.remove());
     }
 }
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let { value } = input
+    let { value } = input;
 
     if (!value) 
-        return alert('Preencha o campo')
+        return alert('Preencha o campo');
 
-    const [name, url] = value.split(",")
+    const [name, url] = value.split(",");
 
     if (!url) 
-        return alert('formate o texto da maneira correta')
+        return alert('formate o texto da maneira correta');
 
     if (!/^http/.test(url)) 
-        return alert("Digite a url da maneira correta")
+        return alert("Digite a url da maneira correta");
 
-    fetch(`http://localhost:3000/?name=${name}&url=${url}`).then(addElement({ name, url }))
+    fetch(`http://localhost:3000/?name=${name}&url=${url}`).then(addElement({ name, url }));
 
-    input.value = ""
+    input.value = "";
 })
